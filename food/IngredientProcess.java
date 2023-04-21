6f742ec484ae0553291c6de99479437f9027c8b0
ghm5755 <ghm5755@g.rit.edu>
2023-04-21 17:55:36 -0400

    Add ingredient by name

--- food/IngredientProcess.java
+++ food/IngredientProcess.java
@@ -4,28 +4,59 @@ import java.io.FileReader;
 import java.io.IOException;  
 
 public class IngredientProcess  {  
-    public static void readIngredients(int arg)   
+    public static Ingredient readIngredientByName(String arg) {
+        String line = "";      
+        try   
+        {  
+        Ingredient thisIngredient = new Ingredient();
+        BufferedReader br = new BufferedReader(new FileReader("./food/ingredients.csv"));  
+        while ((line = br.readLine()) != null)  
+        {  
+            String[] ingredient = line.split(",");   
+            if (ingredient[1].equals(arg)) {
+                for (int i = 0; i < 53; i++) { // there happen to be 53 parts to each ingredient (and != null wasn't working when I tried it)
+                //System.out.println(ingredient[1]); confirmation of running through all
+                thisIngredient = new Ingredient(ingredient); //create new ingredient object
+            }
+            }
+            
+        } 
+        br.close();
+        return thisIngredient;  
+        }   
+        catch (IOException e)   {  
+            e.printStackTrace();  
+            }
+        return null;
+ 
+    }
+    public static Ingredient readIngredientById(int arg)   
     {  
-    String line = "";      
-    String id = Integer.toString(arg);
-    try   
-    {  
-    BufferedReader br = new BufferedReader(new FileReader("./food/ingredients.csv"));  
-    while ((line = br.readLine()) != null)  
-    {  
-        String[] ingredient = line.split(",");   
-        if (ingredient[0].equals(id)) {
-            for (int i = 0; i < 53; i++) { // there happen to be 53 parts to each ingredient (and != null wasn't working when I tried it)
-            //System.out.println(ingredient[i]); confirmation of running through all
-            Ingredient thisIngredient = new Ingredient(ingredient); //create new ingredient object
-        }
-        }
-        
-     }  
-    }   
-    catch (IOException e)   {  
-        e.printStackTrace();  
-        }  
+        String line = "";      
+        String id = Integer.toString(arg);
+        try   
+        {  
+        Ingredient thisIngredient = new Ingredient();
+        BufferedReader br = new BufferedReader(new FileReader("./food/ingredients.csv"));  
+        while ((line = br.readLine()) != null)  
+        {  
+            String[] ingredient = line.split(",");   
+            if (ingredient[0].equals(id)) {
+                for (int i = 0; i < 53; i++) { // there happen to be 53 parts to each ingredient (and != null wasn't working when I tried it)
+                System.out.println(ingredient[1]); //confirmation of running through all
+                thisIngredient = new Ingredient(ingredient); //create new ingredient object
+            }
+            }
+            
+        } 
+        br.close();
+        return thisIngredient;  
+        }   
+        catch (IOException e)   {  
+            e.printStackTrace();  
+            }
+        return null;
+ 
     }  
-}  
+}
 
