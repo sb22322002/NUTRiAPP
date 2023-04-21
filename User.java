2812554a571b3a3c347ec37c3c3be0c9a02eab58
Daniel <95231071+Francuskiii@users.noreply.github.com>
2023-04-21 18:49:41 -0400

    Add files via upload

--- User.java
+++ User.java
@@ -1,54 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.time.LocalDate;  
+import java.time.Period;  
+
+
 public class User {
-    private String name;
-    private int height;
-    private int weight;
-    private String birthDate;
+   String name;
+   double height = 0.0;
+   double weight = 0.0;
+   String goal;
+   String bday; //YYYY-MM-DD format
+   
+   public User(){}
+   
+   public User(String _name, double _height, double _weight, String _goal, String _bday) {
+      this.name = _name;
+      this.height = _height;
+      this.weight = _weight;
+      this.goal = _goal;
+      this.bday = _bday;
+      
+      
+   }
+   
+   //accessors
+   public String getName() { return name; }
+   public double getHeight() { return height; }
+   public double getWeight() { return weight; }
+   public String getGoal() { return goal; }
+   public String getBday() { return bday; }
+   
+   //modifiers
+   public void setName(String _name) { name = _name; } 
+   public void setHeight(double _height) { height = _height; }
+   public void setWeight(double _weight) { weight = _weight; }
+   public void setGoal(String _goal) { goal = _goal; }
+   public void setBday(String _bday) { bday = _bday; }
+   
+   
+   //compute age
+   public static int getAge (String birthdate) {
+      LocalDate dob = LocalDate.parse(birthdate); 
+      LocalDate curDate = LocalDate.now();
+      return Period.between(dob, curDate).getYears();
+   }
+   
     
-    
-    public User(String name, int height, int weight, String birthDate) {
-        this.name = name;
-        this.height = height;
-        this.weight = weight;
-        this.birthDate = birthDate;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public int getHeight() {
-        return height;
-    }
-
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-
-    public int getWeight() {
-        return weight;
-    }
-
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }    
 }
