d12177817e822996ca3e8062b50844be66b78541
Daniel <95231071+Francuskiii@users.noreply.github.com>
2023-04-22 03:13:14 -0400

    Add files via upload

--- User.java
+++ User.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 import java.time.LocalDate;  
 import java.time.Period;  
-
+import java.io.File;
+import java.io.IOException;
 
 public class User {
    String name;
@@ -44,6 +45,26 @@ public class User {
       LocalDate curDate = LocalDate.now();
       return Period.between(dob, curDate).getYears();
    }
+   
+   //Save user info to file
+   public void saveUserInfo() {
+      File saveFile = new File("userData.txt");
+      try {
+         //check if save file exists
+         if (!saveFile.exists()) { saveFile.createNewFile(); }
+      
+         //write to file
+         FileWriter w = new FileWriter(saveFile);
+         w.write(name + "," + height + "," + weight + "," + goal + "," + bday);
+         w.close();
+         System.out.println("Save Successful");
+      }
+      catch (IOException e) {
+         System.out.println("Error: " + e);
+      }
+   }
+   
+
    
     
 }
