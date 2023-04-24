87374ec7fe3967acc393a612368fc098266d65d5
Seth <sb2232@rit.edu>
2023-04-24 01:40:01 -0400

    modified build script

--- runbuild.py
+++ runbuild.py
@@ -1,29 +1,30 @@
 # simple build script
 import os
 err = 0
+classpath = "-classpath \"./food;./commands;./workouts;\""
 try:
     # build main
-    cmd = "javac -classpath \"./food;./commands;./workouts;\" -d ./ ./*.java"
+    cmd = "javac " + classpath + " -d ./ ./*.java"
     err = os.system(cmd)
 
     # build food
     if err == 0:
-        cmd = "javac -d ./food ./food/*.java"
+        cmd = "javac " + classpath + " -d ./food ./food/*.java"
         err = os.system(cmd)
 
     # build commands
     if err == 0:
-        cmd = "javac -d ./commands ./commands/*.java"
+        cmd = "javac " + classpath + " -d ./commands ./commands/*.java"
         err = os.system(cmd)
 
     # build workouts
     if err == 0:
-        cmd = "javac -d ./workouts ./workouts/*.java"
+        cmd = "javac " + classpath +" -d ./workouts ./workouts/*.java"
         err = os.system(cmd)
 
     # run the build
     if err == 0:
-        cmd = "java -classpath \"./commands;./food;\" main"
+        cmd = "java " + classpath + " main"
         os.system(cmd)
     
 except KeyboardInterrupt:
