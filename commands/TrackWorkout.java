0c490e45994f9a8ac44c4044af7349ebd6afbfaf
Seth <sb2232@rit.edu>
2023-04-24 13:11:21 -0400

    save workouts to user

--- commands/TrackWorkout.java
+++ commands/TrackWorkout.java
@@ -11,27 +11,30 @@ public class TrackWorkout implements Command{
      * The constructor initializes an instance of the TrackWorkout class
      * and calls the execute method.
      */
-    public TrackWorkout(){
-        execute();
+    public TrackWorkout(User user){
+        execute(user);
     }
 
     /**
      * The execute method prompts the user for input to track the workout
      * and saves the entered workout information.
      */
-    private void execute(){
+    private void execute(User user){
         Keyboard keyboard = new Keyboard();
         int minutes = keyboard.nextInt("Enter the time of your workout: ");
         String intensity = keyboard.nextWorkoutIntensity("Enter the intensity of your workout (\"high\", \"moderate\", or \"low\"): ");
         switch(intensity){
             case "high":
                 HighIntensityWorkout high_workout = new HighIntensityWorkout(minutes);
+                user.addHighIntensityWorkout(high_workout);
                 break;
             case "moderate":
                 ModerateIntensityWorkout mod_workout = new ModerateIntensityWorkout(minutes);
+                user.addModerateIntensityWorkout(mod_workout);
                 break;
             case "low":
                 LowIntensityWorkout low_workout = new LowIntensityWorkout(minutes);
+                user.addLowIntensityWorkout(low_workout);
                 break;
         }
     }
