a9dee7b2b407f4db426c875e4fc34198316a3f01
ghm5755 <ghm5755@g.rit.edu>
2023-04-24 22:23:13 -0400

    meal/recipe

--- food/Meal.java
+++ food/Meal.java
@@ -1,3 +1,10 @@
 public class Meal {
+    Recipe[] recipes;
+    String instructions;
     
+    Meal(Recipe[] recipes, String instructions) {
+        this.recipes = recipes;
+        this.instructions = instructions; 
+    }
+
 }
--- food/Recipe.java
+++ food/Recipe.java
@@ -1,3 +1,9 @@
 public class Recipe {
-    
+    Ingredient[] ingredients;
+    String instructions;
+
+    Recipe(Ingredient[] ingredients, String instructions) {
+        this.ingredients = ingredients;
+        this.instructions = instructions; 
+    }
 }
