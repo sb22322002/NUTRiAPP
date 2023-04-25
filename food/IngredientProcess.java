e9a52b74ee45ad0c425e9a73df13ea79f9e239b1
ghm5755 <ghm5755@g.rit.edu>
2023-04-24 22:39:37 -0400

    IngredientProcess

--- food/IngredientProcess.java
+++ food/IngredientProcess.java
@@ -12,7 +12,7 @@ public class IngredientProcess  {
         BufferedReader br = new BufferedReader(new FileReader("./food/ingredients.csv"));  
         while ((line = br.readLine()) != null)  
         {  
-            String[] ingredient = line.split(",");   
+            String[] ingredient = line.split(",(?=(?:[^\"]*$)", -1);   
             if (ingredient[1].equals(arg)) {
                 for (int i = 0; ingredient[i] != null; i++) { // there happen to be 53 parts to each ingredient (and != null wasn't working when I tried it)
                 //System.out.println(ingredient[1]); //confirmation of running through all
